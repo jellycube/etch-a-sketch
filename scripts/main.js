da68066a51631fed1fb97633b336f5676b5985ee
Van Lam Tang Thanh <jellycube0710@gmail.com>
2016-08-10 20:05:22 +0700

    grid is working somewhat. Need more testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,17 @@
+var width = 960;
+var height = 960;
+
 for (var i = 0; i < 16; i++) {
 	for (var j = 0; j < 16; j++) {
   	$("section").append("<div class='row'></div>");
+
   }
-  $("section").append("<div class='space'></div>");
 }
+
+    width = 960/16 - 2;
+    height = 960/16 - 2;
+    $(".row").height(height + 'px');
+    $('.row').width(width + 'px');
 
 $('button').on('click', function(){
 	$("section").html("");
@@ -12,7 +20,10 @@ $('button').on('click', function(){
     for (var j = 0; j < size; j++) {
       $("section").append("<div class='row'></div>");
     }
-    $("section").append("<div class='space'></div>");
   }
+      width = 960/size - 2;
+    height = 960/size - 2;
+    $(".row").height(height + 'px');
+    $('.row').width(width + 'px');
   $('section').show();
 });
